dddd38a9bebaf42b47ce7dd62fbed0502b502274
dlegland <david.legland@inrae.fr>
2025-10-03 15:50:52 +0200

    refactor logic for reading data from file

--- src/main/java/net/soleil/psiche/Read_Tomo_Data.java
+++ src/main/java/net/soleil/psiche/Read_Tomo_Data.java
@@ -10,6 +10,9 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.LineNumberReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
@@ -25,6 +28,10 @@ import ij.plugin.PlugIn;
 import ij.process.FloatProcessor;
 
 /**
+ * Opens a file chooser to select a file with ".vol" extension, then parse the
+ * content of the associated ".vol.info" file, and read the content of the data
+ * file.
+ * 
  * @author dlegland
  *
  */
@@ -109,46 +116,53 @@ public class Read_Tomo_Data implements PlugIn
     public void run(String arg)
     {
         // get the file
-        String path = arg;
-        String directory = null;
-        String fileName = null;
+        String pathToFile = arg;
 
-        if (null == path || 0 == path.length())
+        Path filePath = null;
+        if (null == pathToFile || 0 == pathToFile.length())
         {
             File volFile = chooseFileToOpen("Choose .vol file", VOL_FILE_FILTER);
             if (volFile == null) return;
-            directory = volFile.getParent();
-            fileName = volFile.getName();
-            path = directory + "/" + fileName;
+            filePath = volFile.toPath();
         }
         else
         {
-            // the argument is the path
-            File fileIn = new File(path);
-            directory = fileIn.getParent(); // could be a URL
-            fileName = fileIn.getName();
+            // convert input argument into Path object
+            filePath = Paths.get(pathToFile);
         }
 
-        if (!fileName.toLowerCase().endsWith(".vol"))
+        String fileName = filePath.getFileName().toString(); 
+        if (!fileName.endsWith(".vol"))
         {
             System.err.println("Expect input file extension to be '.vol', abort");
             return;
         }
 
         // check existence of vol.info file
-        String infoFileName = fileName + ".info";
-        File file = new File(directory, infoFileName);
-        if (!file.exists())
+        Path pathToInfoFile = filePath.resolveSibling(fileName + ".info");
+        if (!Files.exists(pathToInfoFile))
         {
             System.err.println("Could not find vol.info file, abort");
             return;
         }
 
+        
         // read image data
         ImagePlus imagePlus;
         try
         {
-            imagePlus = readImagePlus(file);
+            ImageInfo info = readImageInfo(pathToInfoFile);
+            
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(filePath.toFile()));
+            
+            ImageStack stack = readImageData(inputStream, info);
+            
+            imagePlus = new ImagePlus(fileName, stack);
+            Calibration calib = new Calibration();
+            calib.pixelWidth = info.pixelSize;
+            calib.pixelHeight = info.pixelSize;
+            calib.pixelDepth = info.pixelSize;
+            imagePlus.setCalibration(calib);
         }
         catch (IOException e)
         {
@@ -165,94 +179,61 @@ public class Read_Tomo_Data implements PlugIn
         stack.getProcessor(currentSliceIndex).resetMinAndMax();
     }
 
-    public ImagePlus readImagePlus(File file) throws IOException
+    private ImageInfo readImageInfo(Path pathToInfoFile) throws IOException
     {
-        // String dataFileName = null;
-        int sizeX = 0;
-        int sizeY = 0;
-        int sizeZ = 0;
-        // int bitDepth = 32;
-        boolean littleEndian = true;
+        try(LineNumberReader reader = new LineNumberReader(new FileReader(pathToInfoFile.toFile())))
+        {
+            ImageInfo info = new ImageInfo();
+            // header line
+            reader.readLine();
 
-        Calibration calib = null;
+            // tokens = valueString.split(" ");
+            // size of the stack
+            String xSizeString = reader.readLine().split("=")[1].trim();
+            info.sizeX = Integer.parseInt(xSizeString);
+            String ySizeString = reader.readLine().split("=")[1].trim();
+            info.sizeY = Integer.parseInt(ySizeString);
+            String zSizeString = reader.readLine().split("=")[1].trim();
+            info.sizeZ = Integer.parseInt(zSizeString);
 
-        LineNumberReader reader = new LineNumberReader(new FileReader(file));
-
-        // header line
-        reader.readLine();
-
-        // tokens = valueString.split(" ");
-        // size of the stack
-        String xSizeString = reader.readLine().split("=")[1].trim();
-        sizeX = Integer.parseInt(xSizeString);
-        String ySizeString = reader.readLine().split("=")[1].trim();
-        sizeY = Integer.parseInt(ySizeString);
-        String zSizeString = reader.readLine().split("=")[1].trim();
-        sizeZ = Integer.parseInt(zSizeString);
-
-        // resolution
-        String resolString = reader.readLine();
-        double resol = Double.parseDouble(resolString.split("=")[1].trim());
-
-        reader.close();
-
-        // determine data file name
-        String fileName = file.getName();
-        String dataFileName = file.getName().substring(0, fileName.length() - 5);
-        System.out.println("read data file: " + dataFileName);
-        File dataFile = new File(file.getParentFile(), dataFileName);
-        IJ.log("read data file: " + dataFile.getAbsolutePath());
-        IJ.log("  Volume size: " + sizeX + "x" + sizeY + "x" + sizeZ);
-
-        // read image data
-        // (assumes all necessary information have been read)
-        ImageStack stack = readData(dataFile, sizeX, sizeY, sizeZ, 32, littleEndian);
-
-        ImagePlus imagePlus = new ImagePlus(file.getName(), stack);
-        calib = new Calibration();
-        calib.pixelWidth = resol;
-        calib.pixelHeight = resol;
-        calib.pixelDepth = resol;
-        imagePlus.setCalibration(calib);
-
-        return imagePlus;
+            // resolution
+            String resolString = reader.readLine();
+            info.pixelSize = Double.parseDouble(resolString.split("=")[1].trim());
+            
+            return info;
+        }
     }
 
-    private ImageStack readData(File file, int sizeX, int sizeY, int sizeZ, int bitDepth, boolean littleEndian) throws IOException
+    private ImageStack readImageData(InputStream inputStream, ImageInfo info) throws IOException
     {
-        // First validates the existence of the file
-        if (!file.exists())
-        { throw new RuntimeException("Could not find data file: " + file.getName()); }
-
         // allocate stack
         IJ.showStatus("Create image stack");
-        ImageStack stack = ImageStack.create(sizeX, sizeY, sizeZ, bitDepth);
+        ImageStack stack = ImageStack.create(info.sizeX, info.sizeY, info.sizeZ, 32);
 
         IJ.showStatus("Read raw data");
 
         // allocate byte array for current slice
-        int pixelsPerPlane = sizeX * sizeY;
-        int nBytes = pixelsPerPlane * 4;
-        byte[] byteData = new byte[nBytes];
+        int pixelsPerPlane = info.sizeX * info.sizeY;
+        int bytesPerPlane = pixelsPerPlane * 4;
+        byte[] byteData = new byte[bytesPerPlane];
 
-        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
         // iterate over slices
-        for (int z = 0; z < sizeZ; z++)
+        for (int z = 0; z < info.sizeZ; z++)
         {
-            IJ.showProgress(z, sizeZ);
+            IJ.showProgress(z, info.sizeZ);
             // read current slice
-            int nRead = inputStream.read(byteData, 0, nBytes);
+            int nRead = inputStream.read(byteData, 0, bytesPerPlane);
 
             // check whole slice was correctly read
-            if (nRead != nBytes)
+            if (nRead != bytesPerPlane)
             {
                 inputStream.close();
-                throw new RuntimeException("Could read only " + nRead + " over the " + nBytes + " expected");
+                throw new RuntimeException("Could read only " + nRead + " over the " + bytesPerPlane + " expected");
             }
 
             // convert byte array to ShortProcessor
-            float[] data = convertToFloatArray(byteData, littleEndian);
-            FloatProcessor slice = new FloatProcessor(sizeX, sizeY, data, null);
+            float[] data = convertToFloatArray(byteData, info.littleEndian);
+            FloatProcessor slice = new FloatProcessor(info.sizeX, info.sizeY, data, null);
 
             stack.setProcessor(slice, z + 1);
         }
@@ -264,38 +245,7 @@ public class Read_Tomo_Data implements PlugIn
 
         return stack;
     }
-
-    // private short[] convertToShortArray(byte[] byteData, boolean
-    // littleEndian)
-    // {
-    // // allocate short array
-    // int size = byteData.length / 2;
-    // short[] data = new short[size];
-    //
-    // // convert byte pairs to shorts
-    // if (littleEndian)
-    // {
-    // for (int i = 0; i < size; i++)
-    // {
-    // byte b1 = byteData[2 * i];
-    // byte b2 = byteData[2 * i + 1];
-    //
-    // int v = ((b2 & 0xFF) << 8 | (b1 & 0x00FF));
-    // data[i] = (short) v;
-    // }
-    // }
-    // else
-    // {
-    // for (int i = 0; i < size; i++)
-    // {
-    // byte b1 = byteData[2 * i];
-    // byte b2 = byteData[2 * i + 1];
-    // data[i] = (short) ((b1 & 0xFF) << 8 | (b2 & 0xFF));
-    // }
-    // }
-    // return data;
-    // }
-
+    
     private float[] convertToFloatArray(byte[] byteData, boolean littleEndian)
     {
         // allocate short array
@@ -327,39 +277,13 @@ public class Read_Tomo_Data implements PlugIn
         }
         return data;
     }
-
-    /**
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException
+    
+    class ImageInfo 
     {
-        System.out.println("Test Read_VGI file");
-
-        String fileName = "Grain_daa11.vgi";
-        File file = new File("D:/images/wheat/perigrain/clermont/demo-tomo/grain_daa11/" + fileName);
-        if (!file.exists())
-        {
-            System.err.println("Could not file file: " + fileName);
-            return;
-        }
-
-        Read_Tomo_Data reader = new Read_Tomo_Data();
-        ImagePlus imagePlus = reader.readImagePlus(file);
-
-        ImageStack stack = imagePlus.getStack();
-        double value0 = stack.getVoxel(0, 0, 0);
-        System.out.println("value at (0,0,0): " + value0);
-        double value1 = stack.getVoxel(55, 0, 0);
-        System.out.println("value at (55,0,0): " + value1);
-        double value2 = stack.getVoxel(200, 100, 300);
-        System.out.println("value at (200,100,300): " + value2);
-        double value3 = stack.getVoxel(0, 0, 5);
-        System.out.println("value at (0,0,5): " + value3);
-        double value4 = stack.getVoxel(0, 0, 10);
-        System.out.println("value at (0,0,10): " + value4);
-        value1 = stack.getVoxel(55, 0, 0);
-        System.out.println("value at (55,0,0): " + value1);
+        int sizeX;
+        int sizeY;
+        int sizeZ;
+        boolean littleEndian = true;
+        double pixelSize;
     }
-
 }
